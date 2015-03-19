1d76e6fc976f8fc4cc46a49475b517a123d7574e
Luís Cleto <luisccleto@gmail.com>
2015-03-19 19:37:09 +0000

    Reset project. Build basic level prototype

---
+++ Source/RobotsVSEngineers/RobotsVSEngineers.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "RobotsVSEngineers.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RobotsVSEngineers, "RobotsVSEngineers" );
---
+++ Source/RobotsVSEngineers/RobotsVSEngineersGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "RobotsVSEngineersGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROBOTSVSENGINEERS_API ARobotsVSEngineersGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
