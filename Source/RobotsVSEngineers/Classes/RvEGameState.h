87e11f3108296e804862a831dee9c65bc5a2b028
Luís Cleto <luisccleto@gmail.com>
2015-04-08 18:07:22 +0100

    Add new character. Create gamestate class.

---
+++ Source/RobotsVSEngineers/Classes/RvEGameState.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameState.h"
+#include "RvEGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROBOTSVSENGINEERS_API ARvEGameState : public AGameState
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
